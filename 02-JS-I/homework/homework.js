d392589f8df4433a5ecac35c46b70e04f0307440
RobertoCHINGUE <robertocarloo2014@hotmail.com>
2022-05-11 16:44:16 -0300

    ok

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,7 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'Hola';
-const nuevaString = 'Buenos dias';
+
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 17;
 
@@ -26,63 +26,66 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  // "Return" la string provista: str
-  // "Amigo"
-  
+  return 'hola amigo';
 }
+devolverString()
+'hola amigo'
 
 function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
-  
+ return x + y; 
 }
+suma (4,9)
+13
 
 function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
-  // Tu código:
-  
+  return x - y;
 }
+resta (4,9)
+-5
 
 function multiplica(x, y) {
-  // Multiplica "x" por "y" y devuelve el valor
-  // Tu código:
-  
+  return x * y;
 }
+multiplica (4,9)
+36
 
 function divide(x, y) {
-  // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
-  
+ return x / y;
 }
+divide (4,9)
+0.444444444444444
 
 function sonIguales(x, y) {
-  // Devuelve "true" si "x" e "y" son iguales
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+ return x == y;
 }
+sonIguales (5,5)
+true
+sonIguales (4,5)
+false
 
 function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+ return str1 == str2;
 }
+tienenMismaLongitud ('leo','leo')
+true
+tienenMismaLongitud ('juan','javier')
+false
 
 function menosQueNoventa(num) {
-  // Devuelve "true" si el argumento de la función "num" es menor que noventa
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+ return num<90
 }
+menosQueNoventa (78)
+true
+menosQueNoventa (100)
+false
 
 function mayorQueCincuenta(num) {
-  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
-  // De lo contrario, devuelve "false"
-  // Tu código:
-  
+  return num>50
 }
+mayorQueCincuenta (89)
+true
+mayorQueCincuenta (43)
+false
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
